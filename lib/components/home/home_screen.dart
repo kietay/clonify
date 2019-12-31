1dcb4ac965f6bf4ef354cee18e0420d8433d03f0
kieran <KieranT94@gmail.com>
2019-12-31 00:37:36 +0000

    Add key for animated switcher

--- lib/components/home/home_screen.dart
+++ lib/components/home/home_screen.dart
@@ -4,24 +4,25 @@ import 'package:clonify/logic/home.dart';
 import 'package:clonify/components/home/recently_played.dart';
 
 final homeScreen = SafeArea(
+    key: ValueKey<int>(1),
     child: ListView(
-  children: <Widget>[
-    Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Material(
-            child: RecentlyPlayed(),
-          )
-          // MadeforYou(),
-          // RecomendedforYou(),
-          // PopularTrending(),
-          // EditorsPicks(),
-          // GlobalReleases(),
-          // BecauseYouLike(),
-        ],
-      ),
-    ),
-  ],
-));
+      children: <Widget>[
+        Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Material(
+                child: RecentlyPlayed(),
+              )
+              // MadeforYou(),
+              // RecomendedforYou(),
+              // PopularTrending(),
+              // EditorsPicks(),
+              // GlobalReleases(),
+              // BecauseYouLike(),
+            ],
+          ),
+        ),
+      ],
+    ));
--- lib/components/home/library.dart
+++ lib/components/home/library.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 final libraryScreen = SafeArea(
+  key: ValueKey<int>(3),
   child: ListView(
     children: <Widget>[Text('Hello ur now libary')],
   ),
